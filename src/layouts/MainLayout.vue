764a575906f18617ffc8046c948d1b9d0c10c822
Noella-68 <noellajarega@gmail.com>
2025-01-08 00:03:02 +0300

    Health and Fitness Landing Page

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf" container style="height: 800px" class="shadow-2 rounded-borders">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-toolbar-title> Health and Fitness App </q-toolbar-title>
+        <span class="q-mr-lg">Set Goal</span>
+
+        About Us
+        <q-space></q-space>
+
+        <span class="q-mr-sm"> <q-btn push color="primary" label="SIGN UP" /> </span>
+        <q-btn push color="primary" label="LOGIN" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+
+
+
+
+
+
+
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-page padding>
+    <q-card flat bordered justify="center">
+      <q-card-section>
+        <div class="text-h6">Track your workouts and see your progress.</div>
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section> Join over 100 million people active on fitness therapy </q-card-section>
+    </q-card>
+
+    <br />
+
+    <div class="row q-gutter-xl justify-center">
+      <q-card flat bordered class="my-card">
+        <q-card-section>
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-h6">Set Goal</div>
+            </q-img>
+
+            <q-card-section>
+              {{ lorem }}
+            </q-card-section>
+            <q-card-actions align="center">
+              <q-btn class="glossy" color="teal" label="Proceed" />
+            </q-card-actions>
+          </q-card>
+        </q-card-section>
+
+        <q-separator inset />
+
+        <q-card-section> Join over 100 million people active on fitness therapy </q-card-section>
+      </q-card>
+
+      <q-card flat bordered class="my-card">
+        <q-card-section>
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-h6">Track Progress</div>
+            </q-img>
+
+            <q-card-section>
+              {{ lorem }}
+            </q-card-section>
+            <q-card-actions align="center">
+              <q-btn class="glossy" color="teal" label="Proceed" />
+            </q-card-actions>
+          </q-card>
+        </q-card-section>
+
+        <q-separator inset />
+
+        <q-card-section> Join over 100 million people active on fitness therapy </q-card-section>
+      </q-card>
+
+      <q-card flat bordered class="my-card">
+        <q-card-section>
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-h6">Diet Plan</div>
+            </q-img>
+
+            <q-card-section>
+              {{ lorem }}
+            </q-card-section>
+            <q-card-actions align="center">
+              <q-btn class="glossy" color="teal" label="Proceed" />
+            </q-card-actions>
+          </q-card>
+        </q-card-section>
+
+        <q-separator inset />
+
+        <q-card-section> Join over 100 million people active on fitness therapy </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const lorem =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
